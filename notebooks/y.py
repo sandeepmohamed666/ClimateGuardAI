9f6dd39aabeb5a916a15e0081668af51b612ca45
sandeepmohamed666@gmai.com <sandeepmohamed666@gmail.com>
2026-06-07 03:13:22 +0530

    Complete EDA phase C6 climate insights report

---
+++ notebooks/y.py
@@ -0,0 +1,20 @@
+# import pandas as pd
+
+
+# df = pd.read_csv("../datasets/raw/IndianWeatherRepository.csv")
+# 
+
+print("Shape")
+# print(df.shape)
+
+print("\nData Types")
+# print(df.dtypes)
+
+print("\nMissing Values")
+# print(df.isnull().sum())
+
+print("\nDuplicates")
+# print(df.duplicated().sum())
+
+print("\nStatistics")
+# print(df.describe())
